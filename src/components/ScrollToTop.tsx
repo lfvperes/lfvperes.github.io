4bb575fd3db72fc47eee6b796cc537be1dfe7154
lfvperes <luisfvperes@gmail.com>
2025-08-12 19:40:52 -0300

    Updates

--- src/components/ScrollToTop.tsx
+++
@@ -1,9 +0,0 @@
-// src/components/ScrollToTop.tsx
-const ScrollToTop = () => {
-  return (
-    <div className='scroll-to-top'>
-      {/* ScrollToTop content will go here */}
-    </div>
-  )
-}
-export default ScrollToTop
