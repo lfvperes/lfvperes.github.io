e260ea4899882940ac05baeb9c14e4315c6acc4e
lfvperes <luisfvperes@gmail.com>
2025-08-12 20:36:35 -0300

    feat: :sparkles: Reactive scroll to top button

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,9 +1,31 @@
-// src/components/ScrollToTop.tsx
+import { useEffect, useState } from 'react';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import './ScrollToTop.css';
+
 const ScrollToTop = () => {
-  return (
-    <div className='scroll-to-top'>
-      {/* ScrollToTop content will go here */}
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, []);
+
+  return isVisible ? (
+    <div className='scroll-top'>
+      <a href='#top' aria-label='top' className='link bg-slate-800'>
+        <ArrowUpwardIcon className='link__icon' fontSize='large' />
+      </a>
     </div>
-  )
-}
-export default ScrollToTop
+  ) : null;
+};
+
+export default ScrollToTop;
